734ee6ec1a495fe9bbf4632a41a4434f76583931
dongwon <kkum9408@gmail.com>
2021-07-27 16:48:47 +0900

    refactor : styled-component로 변환
    - scss에서 styled-components로 변환
    - Global Styles 적용

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,35 @@
-import "./Card.scss";
+import { styled } from "styled-components";
+
+export const CardWrapper = styled.div`
+  padding: 20px;
+  border-radius: 20px;
+  background: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  border-radius: 20px;
+  margin-bottom: 12px;
+
+  & dl {
+    font-size: 18px;
+    display: flex;
+
+    & dt {
+      padding-right: 12px;
+      padding-bottom: 12px;
+      font-weight: bold;
+    }
+  }
+
+  & dl:nth-last-child(1) {
+    display: block;
+    & dt {
+      padding-bottom: 2px;
+    }
+  }
+`;
 
 const Card = ({ comment }) => {
   return (
-    <div className="card">
+    <CardWrapper>
       <dl>
         <dt>Comment Id</dt>
         <dd>{comment.id}</dd>
@@ -15,7 +42,7 @@ const Card = ({ comment }) => {
         <dt>Comment</dt>
         <dd>{comment.body}</dd>
       </dl>
-    </div>
+    </CardWrapper>
   );
 };
 
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,22 @@
 // import Card from "../components/Card"
-import "./CardList.scss";
-
 import Card from "./Card";
 import { comments } from "../lib/dummyData/comments";
+import { styled } from "styled-components";
+
+const CardListWrapper = styled.ul`
+  width: 500px;
+  margin: 33px auto;
+`;
 
 const CardList = () => {
   return (
-    <ul className="cardlist">
+    <CardListWrapper>
       {comments.map((comment) => (
         <li key={comment.id}>
           <Card comment={comment} />
         </li>
       ))}
-    </ul>
+    </CardListWrapper>
   );
 };
 
