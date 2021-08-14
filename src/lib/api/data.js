8fe30700908fc8b83ac054c7d45bc6416f2f24a9
dongwon <kkum9408@gmail.com>
2021-08-15 02:42:52 +0900

    Refactor: fetch 함수 상수 처리
    - BASE_URL, END_POINT, LIMIT 상수 처리

--- src/lib/api/data.js
+++
@@ -1,4 +0,0 @@
-import client from "./client";
-
-export const fetchCommentAPI = (page) =>
-  client.get(`/comments?_page=${page}&_limit=10`);
---
+++ src/uitls/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://jsonplaceholder.typicode.com";
+export const END_POINT = "comments";
+export const LIMIT = 10;
