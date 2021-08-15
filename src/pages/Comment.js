73c242acfa71dfce3b8811571ee2cc76e68d85cd
dongwon <kkum9408@gmail.com>
2021-08-15 12:14:50 +0900

    Refactor: Thottle 이용한 Infinity Scroll 구현
    - 스크롤 이벤트를 이용한 Infiniy Scroll 구현
    - Throttle 이용한 스크롤 이벤트 제어

--- src/pages/Comment.js
+++ src/pages/Comment.js
@@ -1,59 +1,60 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useInView } from "react-intersection-observer";
 
+import {
+  DEBOUNCE_DELAY,
+  DEFAULT_PAGE,
+  LOADING_MESSAGE,
+  NETWORT_ERROR_MESSAGE,
+} from "utils/constants";
 import { CenterText } from "utils/styles/CenterText";
 import { LoadingText } from "utils/styles/LoadingText";
 
 import CommentLists from "components/CommentLists";
 
 import { fetchCommentsRequest } from "reducers/comment";
+import { throttle } from "utils/throttle";
 
 const CommentPage = () => {
-  const [page, setPage] = useState(1);
-  const [ref, inView] = useInView();
+  const [page, setPage] = useState(DEFAULT_PAGE);
   const dispatch = useDispatch();
   const { hasMoreComments, fetchLoading, fetchError } = useSelector(
     (state) => state.comment
   );
 
-  useEffect(() => {
-    if (inView && !fetchLoading && hasMoreComments) {
-      dispatch(fetchCommentsRequest(page));
-
+  const onScroll = useCallback(() => {
+    if (
+      window.scrollY + document.documentElement.clientHeight >
+      document.documentElement.scrollHeight - 300
+    ) {
       const nextPage = page + 1;
       setPage(nextPage);
     }
-  }, [inView, page, fetchLoading, hasMoreComments, dispatch]);
-
-  /*
-  //scroll event를 이용한 무한스크롤 구현
-   useEffect(() => {
-    function onScroll() {
-      if (
-        window.scrollY + document.documentElement.clientHeight >
-        document.documentElement.scrollHeight - 300
-      ) {
-        const nextPage = page + 1;
-        setPage(nextPage);
-      }
-    }
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
   }, [page]);
-  */
+
+  useEffect(() => {
+    if (hasMoreComments) {
+      dispatch(fetchCommentsRequest(page));
+    }
+  }, [page, hasMoreComments, dispatch]);
+
+  useEffect(() => {
+    const onDebounceScroll = throttle(onScroll, DEBOUNCE_DELAY);
+
+    window.addEventListener("scroll", onDebounceScroll);
+    return () => {
+      window.removeEventListener("scroll", onDebounceScroll);
+    };
+  }, [onScroll]);
 
   if (fetchError) {
-    return <CenterText>❌네트워크 오류❌</CenterText>;
+    return <CenterText>{NETWORT_ERROR_MESSAGE}</CenterText>;
   }
 
   return (
     <>
       <CommentLists />
-      <div ref={hasMoreComments && !fetchLoading ? ref : undefined} />
-      {fetchLoading && <LoadingText>⏰Loading...⏰</LoadingText>}
+      {fetchLoading && <LoadingText>{LOADING_MESSAGE}</LoadingText>}
     </>
   );
 };
